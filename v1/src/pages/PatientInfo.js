75dee4247be3027a3487b38fcfa5f03fe3d5628d
Chew Yu Han <yuhanchew@gmail.com>
2023-08-03 17:55:30 +0800

    creating searchbar

--- v1/src/pages/PatientInfo.js
+++ v1/src/pages/PatientInfo.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar/Sidebar';
+import { useState } from "react";
 
+import Sidebar from '../components/Sidebar/Sidebar';
+import SearchBar from '../components/SearchBar/Searchbar';
+import SearchResultsList from '../components/SearchBar/Searchresultlist';
 function PatientInfo() {
+  const [results, setResults] = useState([]);
+
   return (
     <div className='patientinfo'>
       <Sidebar />
       <h1>PatientInfo</h1>
+      <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        {results && results.length > 0 && <SearchResultsList results={results} />}
+      </div>
     </div>
   );
 }
