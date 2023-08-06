7020a5c1193540ae6c34d3d76e5e08339d24188c
Julian Thong <jtho0056@student.monash.edu>
2023-08-06 17:24:07 +0800

    simple table to add, delete and edit patient info without database

---
+++ v1/src/components/PatientTable/Modal.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+import "./Modal.css";
+
+export const Modal = ({ closeModal, onSubmit, defaultValue }) => {
+  const [formState, setFormState] = useState(
+    defaultValue || {
+      page: "",
+      description: "",
+      status: "live",
+    }
+  );
+  const [errors, setErrors] = useState("");
+
+  const validateForm = () => {
+    if (formState.page && formState.description && formState.status) {
+      setErrors("");
+      return true;
+    } else {
+      let errorFields = [];
+      for (const [key, value] of Object.entries(formState)) {
+        if (!value) {
+          errorFields.push(key);
+        }
+      }
+      setErrors(errorFields.join(", "));
+      return false;
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //wont refresh page
+
+    if (!validateForm()) return;
+
+    onSubmit(formState);
+
+    closeModal();
+  };
+
+  return (
+    <div
+      className="modal-container"
+      onClick={(e) => {
+        if (e.target.className === "modal-container") closeModal();
+      }}
+    >
+      <div className="modal">
+        <form>
+          <div className="form-group">
+            <label htmlFor="patientName">Patient Name</label>
+            <input name="patientName" onChange={handleChange} value={formState.patientName} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="contactInfo">Contact</label>
+            <input name="contactInfo" onChange={handleChange} value={formState.contactInfo} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              name="description"
+              onChange={handleChange}
+              value={formState.description}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="status">Status</label>
+            <select
+              name="status"
+              onChange={handleChange}
+              value={formState.status}
+            >
+              <option value="Healthy">Healthy</option>
+              <option value="Stable">Stable</option>
+              <option value="Critical">Critical</option>
+            </select>
+          </div>
+          {errors && <div className="error">{`Please include: ${errors}`}</div>}
+          <button type="submit" className="btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- v1/src/pages/PatientInfo.js
+++ v1/src/pages/PatientInfo.js
@@ -2,9 +2,56 @@ import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar/Sidebar';
 import SearchBar from '../components/SearchBar/Searchbar';
 import SearchResultsList from '../components/SearchBar/Searchresultlist';
+import { Table } from '../components/PatientTable/Table'; // Step 1: Import the Table component
+import { Modal } from '../components/PatientTable/Modal';
 
 function PatientInfo() {
+  const [modalOpen, setModalOpen] = useState(false);
   const [results, setResults] = useState([]);
+
+
+
+  const [rows, setRows] = useState([
+    {
+      page: "Adam",
+      description: "Man",
+      status: "Healthy",
+    },
+    {
+      page: "Eve",
+      description: "Woman",
+      status: "Stable",
+    },
+    {
+      page: "Zeus",
+      description: "Child",
+      status: "Critical",
+    },
+  ]);
+
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
 
   return (
     <div className='patientinfo'>
@@ -15,8 +62,28 @@ function PatientInfo() {
       <div className="search-bar-container">
         <SearchBar setResults={setResults} />
         {results && results.length > 0 && <SearchResultsList results={results} />}
+        {/* Step 3: Include the Table component with the searchResults data */}
+        
       </div>
+      <div className="App">
+      <button onClick={() => setModalOpen(true)} className="button">
+        Add New Patient
+      </button>
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
+    </div>
       
+
+
     </div>
   );
 }
