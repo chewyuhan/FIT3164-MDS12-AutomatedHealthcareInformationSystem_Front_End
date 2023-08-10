2e9887691dfb1af1da2ab1d3816765358b6853e7
Chew Yu Han <yuhanchew@gmail.com>
2023-08-10 15:01:33 +0800

    setting up appointment layout

--- v1/src/pages/Appointment.js
+++ v1/src/pages/Appointment.js
@@ -40,56 +40,58 @@ const Appointment = () => {
   return (
     <div className="appointment-page">
       <Sidebar />
-      <h1>Book an Appointment</h1>
-      <h2>Select a Date</h2>
-      <div className="calendar-container">
-        <Calendar
-          value={appointmentDate}
-          onChange={(date) => setAppointmentDate(date)}
-          onClickDay={handleCalendarClick}
-          tileContent={({ date, view }) => {
-            if (view === 'month') {
-              const formattedDate = date.toISOString().split('T')[0];
-              const appointmentsForDay = appointments.filter(appointment => appointment.date.toISOString().split('T')[0] === formattedDate);
-              const appointmentCount = appointmentsForDay.length;
-              return (
-                <div className="appointment-tile-content">
-                  {appointmentCount >= 0 && <span className="appointment-indicator">📅</span>}
-                  <span className="appointment-count">{appointmentCount}</span>
-                </div>
-              );
-            }
-            return null;
-          }}
-        />
-      </div>
-      <div className="appointment-content">
-        <AppointmentForm
-          doctors={doctors}
-          appointmentDate={appointmentDate}
-          setAppointmentDate={setAppointmentDate}
-          appointmentTime={appointmentTime}
-          setAppointmentTime={setAppointmentTime}
-          selectedDoctorId={selectedDoctorId}
-          setSelectedDoctorId={setSelectedDoctorId}
-          handleSelectAppointmentTime={handleSelectAppointmentTime}
-        />
-      </div>
-      {Array.isArray(selectedAppointment) && selectedAppointment.length > 0 && (
-        <div className="selected-appointment-details">
-          <h2>Appointments for {selectedAppointment[0]?.date.toDateString()}</h2>
-          <div className="centered-list">
-            <ul>
-              {selectedAppointment.map(appointment => (
-                <li key={appointment.time}>
-                  <p>Doctor: {getDoctorNameById(appointment.doctorId)}</p>
-                  <p>Time: {appointment.time}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
+      <div className="appointment-container">
+        <div className="calendar-container">
+          <h1>Book an Appointment</h1>
+          <h2>Select a Date</h2>
+          <Calendar
+            value={appointmentDate}
+            onChange={(date) => setAppointmentDate(date)}
+            onClickDay={handleCalendarClick}
+            tileContent={({ date, view }) => {
+              if (view === 'month') {
+                const formattedDate = date.toISOString().split('T')[0];
+                const appointmentsForDay = appointments.filter(appointment => appointment.date.toISOString().split('T')[0] === formattedDate);
+                const appointmentCount = appointmentsForDay.length;
+                return (
+                  <div className="appointment-tile-content">
+                    {appointmentCount >= 0 && <span className="appointment-indicator">📅</span>}
+                    <span className="appointment-count">{appointmentCount}</span>
+                  </div>
+                );
+              }
+              return null;
+            }}
+          />
         </div>
-      )}
+        <div className="appointment-content">
+          <AppointmentForm
+            doctors={doctors}
+            appointmentDate={appointmentDate}
+            setAppointmentDate={setAppointmentDate}
+            appointmentTime={appointmentTime}
+            setAppointmentTime={setAppointmentTime}
+            selectedDoctorId={selectedDoctorId}
+            setSelectedDoctorId={setSelectedDoctorId}
+            handleSelectAppointmentTime={handleSelectAppointmentTime}
+          />
+        </div>
+        {Array.isArray(selectedAppointment) && selectedAppointment.length > 0 && (
+          <div className="selected-appointment-details">
+            <h2>Appointments for {selectedAppointment[0]?.date.toDateString()}</h2>
+            <div className="centered-list">
+              <ul>
+                {selectedAppointment.map(appointment => (
+                  <li key={appointment.time}>
+                    <p>Doctor: {getDoctorNameById(appointment.doctorId)}</p>
+                    <p>Time: {appointment.time}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
