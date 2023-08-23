f26616e62d0dd0eaf4901a32bd437d0577434651
Chew Yu Han <yuhanchew@gmail.com>
2023-08-23 16:37:13 +0800

    create patient diag interface

--- v1/src/pages/PatientDiag.js
+++ v1/src/pages/PatientDiag.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar/Sidebar';
+import PatientDiagnosisForm from '../components/PatientDiagForm/DiagForm';
 
 function PatientDiag() {
   return (
     <div className='patientdiag'>
       <Sidebar />
       <h1>PatientDiag</h1>
+      {/* <PatientDiagnosisForm /> Insert the PatientDiagnosisForm component here */}
     </div>
   );
 }
