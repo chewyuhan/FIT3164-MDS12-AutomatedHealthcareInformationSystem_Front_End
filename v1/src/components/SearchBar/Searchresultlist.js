0284a8af215b5c05398de1c6843c57783ab80a3c
Chew Yu Han <yuhanchew@gmail.com>
2023-09-06 22:55:36 +0800

    setting searchbar api get request

--- v1/src/components/SearchBar/Searchresultlist.js
+++ v1/src/components/SearchBar/Searchresultlist.js
@@ -5,7 +5,7 @@ const SearchResultsList = ({ results }) => {
   return (
     <div className="results-list">
       {results.map((result, id) => {
-        return <SearchResult result={result.name} key={id} />;
+        return <SearchResult result={`${result.firstName} ${result.lastName}`} key={id} />;
       })}
     </div>
   );
