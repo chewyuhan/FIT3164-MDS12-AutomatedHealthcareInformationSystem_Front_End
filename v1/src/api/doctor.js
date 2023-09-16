fb695e220260b80c6fde9d0ca4f734cd5bcf6c51
Chew Yu Han <yuhanchew@gmail.com>
2023-09-17 00:00:43 +0800

    create doctor api

---
+++ v1/src/api/doctor.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+// Function to fetch diagnosis data from the API
+
+export const fetchDoctorDataFromAPI = async () => {
+    const accessToken = sessionStorage.getItem("accessToken");
+
+    if (!accessToken) {
+        // Handle the case where there's no access token (authentication failed)
+        console.error("No access token found");
+        return;
+    }
+    try {
+        const response = await axios.get(`https://mds12.cyclic.app/employees/doctors`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data from the API:", error);
+        return [];
+    }
+};
