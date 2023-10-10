200e11783b05e900f4ca3764437753d83c8d7b8e
Chew Yu Han <yuhanchew@gmail.com>
2023-10-10 21:19:49 +0800

    change idioms

--- src/components/Dashboard/PatientCreationHistory.js
+++ src/components/Dashboard/PatientCreationHistory.js
@@ -6,6 +6,7 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function PatientCreationHistory() {
   const [patientData, setPatientData] = useState([]);
+  const [selectedInterval, setSelectedInterval] = useState('daily'); // Default to daily view
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,7 +14,7 @@ function PatientCreationHistory() {
         const data = await fetchPatientDataFromAPI();
         setPatientData(data);
       } catch (error) {
-        console.error('Error fetching patient data:', error);
+        console.error('Error fetching patient data:', error); 
       }
     };
 
@@ -24,7 +25,27 @@ function PatientCreationHistory() {
     const countMap = {};
 
     patientData.forEach((patient) => {
-      const date = new Date(patient.createdAt).toLocaleDateString();
+      let date;
+
+      switch (selectedInterval) {
+        case 'weekly':
+          date = new Date(patient.createdAt);
+          date.setHours(0, 0, 0, 0);
+          date.setDate(date.getDate() - date.getDay()); // Start of the week
+          break;
+        case 'monthly':
+          date = new Date(patient.createdAt);
+          date.setHours(0, 0, 0, 0);
+          date.setDate(1); // Start of the month
+          break;
+        case 'yearly':
+          date = new Date(patient.createdAt);
+          date.setHours(0, 0, 0, 0);
+          date.setMonth(0, 1); // Start of the year
+          break;
+        default:
+          date = new Date(patient.createdAt).toLocaleDateString();
+      }
 
       if (countMap[date]) {
         countMap[date]++;
@@ -53,15 +74,29 @@ function PatientCreationHistory() {
     },
     data: [
       {
-        type: 'line',
+        type: 'column',
+        color: 'rgba(0,75,141,0.7)',
         dataPoints: countPatientsByDate(),
       },
     ],
   };
 
+  const handleIntervalChange = (event) => {
+    setSelectedInterval(event.target.value);
+  };
+
   return (
     <div>
       <h2>Patient Creation History</h2>
+      <div>
+        <label htmlFor="interval">Select Interval: </label>
+        <select id="interval" value={selectedInterval} onChange={handleIntervalChange}>
+          <option value="daily">Daily</option>
+          <option value="weekly">Weekly</option>
+          <option value="monthly">Monthly</option>
+          <option value="yearly">Yearly</option>
+        </select>
+      </div>
       <CanvasJSChart options={options} />
     </div>
   );
